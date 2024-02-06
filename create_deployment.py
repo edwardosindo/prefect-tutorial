042ce663a74bfe4ec6fb389f39bdd07aea453ab7
Edward Osindo <eddu.almasi@gmail.com>
2024-02-06 10:51:38 +0300

    first commit

---
+++ create_deployment.py
@@ -0,0 +1,16 @@
+# Make your code schedulable
+
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="",
+        entrypoint="prefect_tutorial.py:repo_info",
+
+    ).deploy(
+        name="my-first-deployment",
+        work_pool_name="edward-managed-pool",
+        cron="0 1 * * * ",
+    )
+
+    
